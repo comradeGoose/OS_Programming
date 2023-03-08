8b55b56c60c6e4c6b5d87cb34dc6941b4620f95e
Grebenyuk Alexey <78959401+comradeGoose@users.noreply.github.com>
2023-03-08 22:49:47 +0900

    edit

--- ticket_office/main.cpp
+++ ticket_office/main.cpp
@@ -25,11 +25,11 @@ int main()
     }
 
     // определяем скидку в зависимости от возраста самого младшего члена группы
-    float discount = 1 - (discount / 100);
+    float discount = 1 - (min_age / 100);
 
     // вычисляем общую стоимость билетов
     total_cost = TICKET_PRICE * 5 * discount;
-    
+
     cout << "Общая стоимость билетов: " << total_cost << " долларов." << endl;
 
     return 0;
